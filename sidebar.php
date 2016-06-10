a3d6afd1147b44a49d791d427ad98ef6b4b1b0bf
laras126 <laras126@gmail.com>
2016-06-10 10:05:42 -0400

    add layout classes to sidebar

--- sidebar.php
+++ sidebar.php
@@ -12,6 +12,6 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<aside id="secondary" class="widget-area" role="complementary">
+<aside id="secondary" class="widget-area col-xs-12 col-sm-3 col-md-4" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->

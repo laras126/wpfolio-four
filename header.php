55d94f9187a276a48e53d757b7b4eb97844ea071
laras126 <laras126@gmail.com>
2016-06-10 10:06:15 -0400

    add wrapper to site

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
+<div id="page" class="site wrapper">
 	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'wpfolio' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
